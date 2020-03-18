466e0059bf6b09320be879e5da400924804cec94
VadimSaltykov <62356234+VadimSaltykov@users.noreply.github.com>
2020-03-18 23:27:26 +0300

    Add files via upload

---
+++ springhw/src/main/java/springhw/App.java
@@ -0,0 +1,17 @@
+package springhw;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springhw.beans.HelloBean;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Создаем контекст приложения из xml файла ("src/main/resources/applicationContext.xml")
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // Считываем бин (объект)
+        HelloBean hb = ctx.getBean("firstBean", HelloBean.class);
+        // Печатаем объект в консоль 
+        System.out.println(hb);
+	}
+
+}
