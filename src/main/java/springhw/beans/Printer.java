c3ecc3632b16520f442857a327cfb57e0022b959
Vadim Saltykov <saltykov-vadik@mail.ru>
2020-03-19 10:27:24 +0300

    Убран Singleton

--- src/main/java/springhw/beans/Printer.java
+++ src/main/java/springhw/beans/Printer.java
@@ -1,19 +1,8 @@
 package springhw.beans;
 
 public class Printer {
-    private static Printer instance = null;
 
-    private Printer() {
-
-    }
-
-    public static Printer getInstance() {
-        if (instance == null)
-            instance = new Printer();
-        return instance;
-    }
-
-    public void print(Object object) {
-        System.out.println(object);
+    public void print(HelloBean hb) {
+        System.out.println(hb.toString());
     }
 }
